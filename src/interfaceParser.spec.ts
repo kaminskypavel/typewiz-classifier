a08f06d8c3bdbced5b3bf93d806a609503e5e5be
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2018-06-17 17:07:35 +0300

    feat: add initial parsing skills and tests

---
+++ src/interfaceParser.spec.ts
@@ -0,0 +1,38 @@
+import {sanitizeDeclaration} from './interfaceParser';
+
+describe('#sanitizeDeclaration', () => {
+	it('should remove the word "export"', () => {
+		expect(sanitizeDeclaration('export interface A {}')).toEqual('interface A {}');
+	});
+
+	it('should should remove comments', () => {
+		const example = `interface Field {
+        /**
+         * Enable sorting for the field
+         */
+        allowSort?: boolean;
+        /**
+         * Name of the field
+         */
+        fieldName: string;
+        /**
+         * Type of the field
+         */
+        fieldType?: Field.FieldTypeEnum;
+        /**
+         * Analyzer to be used during indexing
+         */
+        indexAnalyzer?: string;
+        /**
+         * Analyzer to be used during searching
+         */
+        searchAnalyzer?: string;
+        /**
+         * Similarity defines the components of scoring. Similarity determines how \\r\\nengine weights terms. FlexSearch interacts with Similarity at both index-time \\r\\nand query-time.
+         */
+        similarity?: Field.SimilarityEnum;
+    }`;
+
+		expect(sanitizeDeclaration(example)).toMatchSnapshot();
+	});
+});
---
+++ src/interfaceParser.ts
@@ -0,0 +1,13 @@
+export const sanitizeDeclaration = (delcaration: string) => {
+	let res = delcaration;
+
+	// remove export keyword
+	if (delcaration.indexOf('export') === 0) {
+		res = delcaration.substring(7, delcaration.length);
+	}
+
+	// remove comments
+	res = delcaration.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, '');
+
+	return res;
+};
